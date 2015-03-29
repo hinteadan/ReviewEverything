5d752847c127806612cb8ba61c6528e3a7240706
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-03-30 00:44:58 +0300

    Play around with HTML Agility Pack

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Playground
     {
         static void Main(string[] args)
         {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.Load(@"C:\Users\hinte_000\Downloads\Telefon mobil Nokia 930 Lumia, 32GB, Black - eMAG.ro.htm");
+
+            var productInfoArea = htmlDoc.GetElementbyId("product-info");
+
+            var name = productInfoArea.Descendants().Single(n => n.Id == "offer-title").Descendants("h1").Single().InnerText;
+
         }
     }
 }
