afd99abc9824cf08f57b2c6bda2bac0e62af46ad
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-03-30 16:32:12 +0300

    Cleanup Playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -16,14 +16,6 @@ namespace Playground
             var result = new CelRoSearch().SearchFor(new SearchCriteria("Nokia Lumia 930"));
 
             var item = result.First().Parse();
-
-            //HtmlDocument htmlDoc = new HtmlDocument();
-            //htmlDoc.Load(@"C:\Users\hinte_000\Downloads\Telefon mobil Nokia 930 Lumia, 32GB, Black - eMAG.ro.htm");
-
-            //var productInfoArea = htmlDoc.GetElementbyId("product-info");
-
-            //var name = productInfoArea.Descendants().Single(n => n.Id == "offer-title").Descendants("h1").Single().InnerText;
-
         }
     }
 }
