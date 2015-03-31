486e5617b615ebc41ff311ac7798e12716482d81
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-03-31 14:39:39 +0300

    Draft data persistence interface

---
+++ ReviewEverything.Model/ICanStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewEverything.Model
+{
+    public interface ICanStore : ICanBeSearched
+    {
+        void Persist(SearchCriteria criteria, IEnumerable<ReviewItem> items);
+        ReviewItem Retrieve(Uri reference);
+    }
+}
