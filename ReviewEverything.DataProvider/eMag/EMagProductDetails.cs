614af88dcdf73d0ccda82a503507004e691db679
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-04-01 14:45:11 +0300

    Scaffold eMag crawler classes

---
+++ ReviewEverything.DataProvider/eMag/EMagProductDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReviewEverything.Model;
+
+namespace ReviewEverything.DataProvider.eMag
+{
+    public class EMagProductDetails : ICanBeParsed
+    {
+        public ReviewItem Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReviewEverything.DataProvider/eMag/EMagSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReviewEverything.Model;
+
+namespace ReviewEverything.DataProvider.eMag
+{
+    public class EMagSearch : ICanBeSearched
+    {
+
+        public IEnumerable<ICanBeParsed> SearchFor(SearchCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
